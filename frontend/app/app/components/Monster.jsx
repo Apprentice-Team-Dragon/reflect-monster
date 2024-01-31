8e095da4912e56f5f39d365d15367902c82fc08e
makoto hata <makotohata0429@gmail.com>
2024-01-31 22:42:14 +0900

    モンスターコンポーネントの画像をImageコンポーネントに変更

--- frontend/app/app/components/Monster.jsx
+++ frontend/app/app/components/Monster.jsx
@@ -1,7 +1,13 @@
+'use client'
+import Image from "next/image";
+
 export default function Monster() {
+
   return (
     <div className="monster-container">
-      <img src="img/firstegg.png" alt="卵" />
+      <Image src={"/img/firstegg.png"} alt="卵" width={500} height={500}/>
+      {/* <Image src={process.env.NEXT_PUBLIC_STRAGE_URL/monster.image} alt="卵" width={500} height={500}/> */}
+      {/* <Image src={`${process.env.NEXT_PUBLIC_STRAGE_URL}/1706632518363_amg8wzx.png`} alt="卵" width={500} height={500}/> */}
     </div>
   );
 }
