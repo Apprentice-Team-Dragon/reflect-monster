2b1a0b9e10a72b98e2ec9c698cab63808e341358
makoto hata <makotohata0429@gmail.com>
2024-01-31 22:42:32 +0900

    test用file

--- frontend/app/app/test/page.jsx
+++ frontend/app/app/test/page.jsx
@@ -1,44 +1,22 @@
 "use client";
 import useFetch from "@/hooks/useFetch";
 import useModal from "@/hooks/useModal";
-import {
-  generateMonster,
-  getGoalCategory,
-  removeBackground,
-} from "@/utils/api/monster";
+import { generateMonster } from "@/utils/api/monster";
 import { useIsEvolve } from "@/hooks/MonsterHooks/monster";
-import { useCallback } from "react";
 import Image from "next/image";
 import { useState, useEffect } from "react";
-// import { uploadImage } from "@/utils/strage";
 import Monster from "../components/Monster"
 import {useGetMonster} from "@/hooks/MonsterHooks/useGetMonster"
 
 
 export default function Test() {
-  // const url = "https://api.stability.ai/v1/user/balance";
-  // const config = {
-  //   method: "GET",
-  //   headers: {
-  //     Authorization: "Bearer " + process.env.NEXT_PUBLIC_STABILITY_API_KEY,
-  //     "Content-Type": "application/json",
-  //   },
-  // };
-  // const { data, isLoading, hasError, errorMessage } = useFetch(url, {
-  //   ...config,
-  // });
-  // const { modalStatus, closeModal, openModal } = useModal();
 
+  const { modalStatus, closeModal, openModal } = useModal();
 
   const { data, isLoading, hasError, errorMessage } = useGetMonster(1);
-  const { monster, setMonster } = useState(null);
-  useEffect(() => {
-    const hundleMonster = ()=> {
-      setMonster((prevMonster) => data.monster)
-    }
-  }, [])
 
-  console.log(monster)
+
+  console.log(data)
 
   if (isLoading) {
     return <div>Loading...</div>;
@@ -49,7 +27,8 @@ export default function Test() {
     return <div>{error}</div>;
   }
 
-  const result = data?.credits;
+  // const result = data?.credits;
+  const result = data?.monster.image;
   return (
     <div>
       {result}
@@ -57,7 +36,8 @@ export default function Test() {
         <button onClick={() => openModal()}>Open</button>
       </div>
       <ModalComponent modalStatus={modalStatus} closeModal={closeModal} />
-      <Monster />
+      {/* <Monster {...data.monster}/> */}
+      <Monster {...data.monster}/>
     </div>
   );
 }
@@ -89,8 +69,6 @@ const ModalComponent = ({ modalStatus, closeModal }) => {
   async function hundleEvolve() {
     setLoading(true);
     const image = await generateMonster("運動する 毎日10kmランニングする", monsterInfo);
-    // const image = await removeBackground(base64);
-    // const image = await uploadImage(base64);
     setLoading(false);
     setMonterImage(() => image);
     setPoint(0);
@@ -102,7 +80,6 @@ const ModalComponent = ({ modalStatus, closeModal }) => {
     <div>
       <p>ここはコンテンツです。</p>
       <button onClick={() => closeModal()}>Close</button>
-      {/* <button onClick={() => hundleGenerateMonster()}>モンスター生成</button> */}
       <div>{point}</div>
       <button onClick={() => hundlePoint()}>ポイント追加</button>
       {isEvolve ? (
