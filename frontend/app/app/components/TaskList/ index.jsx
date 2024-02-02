7cf86c5c9f655f3232e632880e77aa4a76ff5a25
HACHI3232 <harada.chisato526@gmail.com>
2024-02-03 02:11:35 +0900

    タスクのチェックの状態を取得

--- frontend/app/app/components/TaskList/ index.jsx
+++ frontend/app/app/components/TaskList/ index.jsx
@@ -1,14 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './style.scss';
 
-// TaskItem コンポーネントの定義
-const TaskItem = ({ id, label }) => (
-  <div className="check-container">
-    <input type="checkbox" id={id} />
-    <label htmlFor={id}></label>
-    <span className="tag">{label}</span>
-  </div>
-);
+const TaskItem = ({ id, label }) => {
+  // チェックされているかの状態を管理するためのuseStateフック
+  const [isChecked, setIsChecked] = useState(false);
+
+  // チェックボックスの変更をハンドルし、状態変更をログに出力する関数
+  const handleCheckboxChange = () => {
+    const newCheckedState = !isChecked;
+    setIsChecked(newCheckedState); // 状態を更新
+    // チェックボックスの新しい状態をコンソールにログ出力
+    console.log(`タスク ${id} は ${newCheckedState ? "チェックされました" : "チェックが外されました"}`);
+  };
+
+  return (
+    <div className="check-container">
+      <input
+        type="checkbox"
+        id={id}
+        checked={isChecked}
+        onChange={handleCheckboxChange}
+      />
+      <label htmlFor={id}></label>
+      <span className="tag">{label}</span>
+    </div>
+  );
+};
+
 
 // TaskList コンポーネントの定義
 const TaskList = () => {
@@ -19,8 +37,6 @@ const TaskList = () => {
           <h2>左側のタスクリスト</h2>
           <div className="container">
             <TaskItem id="one" label="Choice One" />
-            <TaskItem id="two" label="Choice Two" />
-            <TaskItem id="three" label="Choice Three" />
           </div>
         </div>
       </div>
@@ -29,12 +45,12 @@ const TaskList = () => {
       </div>
       <div className="main-content-task-right">
         <div className="right-task-contents">
-          <h2>右側のタスクリスト</h2>
+          {/* <h2>右側のタスクリスト</h2>
           <div className="container">
             <TaskItem id="four" label="Choice One" />
             <TaskItem id="five" label="Choice Two" />
             <TaskItem id="six" label="Choice Three" />
-          </div>
+          </div> */}
         </div>
       </div>
     </div>
