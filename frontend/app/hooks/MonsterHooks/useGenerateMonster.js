49a00042d16d2bbcaff74af81b86f9749bdefab6
makoto hata <makotohata0429@gmail.com>
2024-02-03 12:51:31 +0900

    不要なコードを削除

--- frontend/app/hooks/MonsterHooks/useGenerateMonster.js
+++ frontend/app/hooks/MonsterHooks/useGenerateMonster.js
@@ -1,30 +1,27 @@
 import { useState, useEffect } from "react";
 import { generateMonster } from "../../utils/api/monster";
-import {
-  getMonster,
-  addExpPoint,
-  getGoalText,
-  postMonster,
-} from "../../utils/api/monsterApi";
-import { getCredit } from "../../utils/api/monster";
+import { getGoalText, postMonster } from "../../utils/api/monsterApi";
 
-// export function useGenerateMonster(goalId, monsterInfo, isEvolve) {
-export function useGenerateMonster(monster, hundleMonsterImage, hundleMonsterExpPoint) {
+export function useGenerateMonster(
+  monster,
+  hundleMonsterImage,
+  hundleMonsterExpPoint
+) {
   const [isGenerating, setIsGenerating] = useState(false);
   const [hasGenerateError, setHasGenerateError] = useState(false);
 
   useEffect(() => {
     const hundleGenerateMonster = async () => {
       // TODO ここで、モンスターの経験値をデータベースに保存 DBの値を使って進化できるか判定する
-      setIsGenerating(true)
+      setIsGenerating(true);
       try {
-        const goalContent = await getGoalText(2)
+        const goalContent = await getGoalText(2);
         const { imagePath, seed } = await generateMonster(goalContent, monster);
 
         hundleMonsterImage(imagePath);
         hundleMonsterExpPoint(0);
         postMonster(monster.id, imagePath);
-        console.log(seed)
+        console.log(seed);
       } catch (error) {
         setHasGenerateError(true);
       } finally {
@@ -34,13 +31,12 @@ export function useGenerateMonster(monster, hundleMonsterImage, hundleMonsterExp
     if (monster.exp_point >= monster.max_exp_point) {
       console.log(`id=${monster.id}のモンスターを進化します`);
       hundleGenerateMonster();
-      // hundleMonsterImage("1706632518363_amg8wzx.png")
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [monster]);
   const generateMonsterInfo = {
     isGenerating,
-    hasGenerateError
+    hasGenerateError,
   };
   return { generateMonsterInfo };
 }
