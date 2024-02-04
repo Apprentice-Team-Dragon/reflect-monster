d4875757bbbe4df1f58d5ecde3d722ff0c6ba90e
makoto hata <makotohata0429@gmail.com>
2024-02-04 17:58:04 +0900

    fix: モンスター生成時にstateのmonsterがDBと不整合であった不具合を修正

--- frontend/app/hooks/MonsterHooks/useGenerateMonster.js
+++ frontend/app/hooks/MonsterHooks/useGenerateMonster.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { generateMonster } from "../../utils/api/monster";
-import { getGoalText, postMonster } from "../../utils/api/monsterApi";
+import { getGoalText, getMonster, postMonster } from "../../utils/api/monsterApi";
 
 export function useGenerateMonster(
   monster,
@@ -16,12 +16,14 @@ export function useGenerateMonster(
       // TODO ここで、モンスターの経験値をデータベースに保存 DBの値を使って進化できるか判定する
       setIsGenerating(true);
       try {
+        const monster = await getMonster();
         const goalContent = await getGoalText(goalId);
         const { imagePath, seedValue } = await generateMonster(goalContent, monster);
 
         hundleMonsterImage(imagePath);
         hundleMonsterExpPoint(0);
         postMonster(monster.id, imagePath, seedValue);
+        // ここでseed値をmonster stateに入れる必要がある
         console.log(seedValue);
 
       } catch (error) {
